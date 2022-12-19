8365c135253f170fabd0e86620f83115728bdf5b
Charles <charlesferlet@gmail.com>
2022-12-19 23:54:27 +0100

    Update admin.py

--- epicevents/app/admin.py
+++ epicevents/app/admin.py
@@ -1,2 +1,9 @@
 from django.contrib import admin
 
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "role", "is_staff", "is_admin", "is_superuser")
+    list_filter = ("is_admin", "role", "groups")
