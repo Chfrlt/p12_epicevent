008e06b5ee9615be82225eb6b8bbd3f4b13f3435
Charles <charlesferlet@gmail.com>
2022-12-20 00:01:45 +0100

    feat: added a class DualSerializerViewSet in views to allow for multiple serializers per ModelViewSet

--- epicevents/app/views.py
+++ epicevents/app/views.py
@@ -1,8 +1,7 @@
 from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-from .serializers import RegisterSerializer
+from rest_framework.viewsets import ModelViewSet
 
 
 class RegisterAPIView(APIView):
@@ -14,3 +13,13 @@ class RegisterAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class DualSerializerViewSet(ModelViewSet):
+    
+    detail_serializer_class = None
+
+    def get_serializer_class(self):
+        detail_serializer_actions = ['retrieve', 'update', 'partial_update', 'create']
+        if self.action in detail_serializer_actions and self.detail_serializer_class is not None:
+            return self.detail_serializer_class
+        return super().get_serializer_class()
