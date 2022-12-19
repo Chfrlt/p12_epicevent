7b75829a148cd1a23065a6f8b0cccfa5c65451aa
Charles <charlesferlet@gmail.com>
2022-12-20 00:27:05 +0100

    feat: added urls for Client request

--- epicevents/app/urls.py
+++ epicevents/app/urls.py
@@ -1,16 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
-from .views import RegisterAPIView
+from rest_framework.routers import DefaultRouter
 
+from .views import RegisterAPIView, ClientViewset
+
+
+clients_router  = DefaultRouter()
+clients_router.register("clients", ClientViewset, basename="clients")
 
 urlpatterns = [
     path('signup/', RegisterAPIView.as_view()),
-    path(
-        'login/', jwt_views.TokenObtainPairView.as_view(),
-        name='login'
-        ),
-    path(
-        'api/token/refresh/', jwt_views.TokenRefreshView.as_view(),
-        name='token_refresh'
-        )
+    path('login/', jwt_views.TokenObtainPairView.as_view(),
+         name='login'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('', include(clients_router.urls))
     ]
