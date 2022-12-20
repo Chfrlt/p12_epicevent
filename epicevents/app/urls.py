6ff17e8eaa10bf3315c1d15b7fffab4b13e57c07
Charles <charlesferlet@gmail.com>
2022-12-20 02:42:19 +0100

    feat: added urls for Contract request

--- epicevents/app/urls.py
+++ epicevents/app/urls.py
@@ -2,11 +2,14 @@ from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
 from rest_framework.routers import DefaultRouter
 
-from .views import RegisterAPIView, ClientViewset
+from .views import (RegisterAPIView, ClientViewset,
+                    ContractViewset)
 
 
 clients_router  = DefaultRouter()
 clients_router.register("clients", ClientViewset, basename="clients")
+contracts_router  = DefaultRouter()
+contracts_router.register("contracts", ContractViewset, basename="contracts")
 
 urlpatterns = [
     path('signup/', RegisterAPIView.as_view()),
@@ -14,5 +17,6 @@ urlpatterns = [
          name='login'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(),
          name='token_refresh'),
-    path('', include(clients_router.urls))
+    path('', include(clients_router.urls)),
+    path('', include(contracts_router.urls))
     ]
