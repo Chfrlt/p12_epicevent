a1b45ad0afa7d542b81487bf261c84fdd9ec9660
Charles <charlesferlet@gmail.com>
2022-12-28 09:01:21 +0100

    feat: Add custom permissions for manager users

--- epicevents/app/permissions.py
+++ epicevents/app/permissions.py
@@ -1,2 +1,10 @@
 from rest_framework import permissions
 from rest_framework.exceptions import PermissionDenied
+
+
+class IsManager():
+    def has_permission(self, request, view):
+        return (request.user.role == 1)
+
+    def has_object_permission(self, request, view, obj):
+        return (request.user.role == 1)
