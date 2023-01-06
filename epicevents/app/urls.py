4848786c9017d94ba206cd439568d9d4c043e0dc
Charles <charlesferlet@gmail.com>
2023-01-06 11:24:54 +0100

    fix: Fix EventViewset import error
    This fix EventViewSet missing as a module import.

--- epicevents/app/urls.py
+++ epicevents/app/urls.py
@@ -3,20 +3,23 @@ from rest_framework_simplejwt import views as jwt_views
 from rest_framework.routers import DefaultRouter
 
 from .views import (RegisterAPIView, ClientViewset,
-                    ContractViewset)
+                    ContractViewset, EventViewset)
 
 
 clients_router  = DefaultRouter()
 clients_router.register("clients", ClientViewset, basename="clients")
 contracts_router  = DefaultRouter()
 contracts_router.register("contracts", ContractViewset, basename="contracts")
+events_router = DefaultRouter()
+events_router.register("events", EventViewset, basename='events')
 
 urlpatterns = [
     path('signup/', RegisterAPIView.as_view()),
     path('login/', jwt_views.TokenObtainPairView.as_view(),
          name='login'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(),
          name='token_refresh'),
     path('', include(clients_router.urls)),
-    path('', include(contracts_router.urls))
+    path('', include(contracts_router.urls)),
+    path('', include(events_router.urls))
     ]
