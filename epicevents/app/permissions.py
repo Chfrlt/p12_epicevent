090e280905866c85003478480317eb2fb0d46574
Charles <charlesferlet@gmail.com>
2023-01-16 01:02:39 +0100

    feat: Add permissions for client model

--- epicevents/app/permissions.py
+++ epicevents/app/permissions.py
@@ -11,5 +11,20 @@ class IsManager():
     def has_object_permission(self, request, view, obj):
         return request.user.role == 1
 
+
+class ClientPermissions(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            return request.user.role == User.SALES
+        return request.user.role == 3 or request.user.role == 2
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in ('PATCH', 'PUT') or request.method in permissions.SAFE_METHODS:
+            if request.user.role == 2:
+                    return True
+            if request.user.role == 3:
+                return (obj in [c.id for c in Client.objects
+                                              .filter(contract__event__support_contact=request.user)])
+
     def has_object_permission(self, request, view, obj):
         return (request.user.role == 1)
