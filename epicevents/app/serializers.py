b5e9f9189297f56dcae35eea77df3f386b0b2579
Charles <charlesferlet@gmail.com>
2023-01-16 01:01:31 +0100

    fix: Fix import error

--- epicevents/app/serializers.py
+++ epicevents/app/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.hashers import make_password
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ValidationError
 from django.contrib.auth.password_validation import validate_password
 
 from .models import User, Event, Client, Contract
@@ -30,7 +30,6 @@ class UserDetailSerializer(ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def validate_password(self, value):
-        """Hashes password."""
         if len(value) < 8:
             raise ValidationError('Le mot de passe doit contenir au moins 8 caractères')
         return make_password(value)
