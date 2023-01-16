09ee5ed00317e24984ddea9fab64e5f61703c7f1
Charles <charlesferlet@gmail.com>
2023-01-16 01:02:55 +0100

    feat: Add permissions for Contract model

--- epicevents/app/permissions.py
+++ epicevents/app/permissions.py
@@ -26,5 +26,24 @@ class ClientPermissions(permissions.BasePermission):
                 return (obj in [c.id for c in Client.objects
                                               .filter(contract__event__support_contact=request.user)])
 
+
+class ContractPermissions(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            return request.user.role == 2
+        return request.user.role == 3 or request.user.role == 2
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in ('PATCH', 'PUT') or request.method in permissions.SAFE_METHODS:
+            if request.user.role == 2:
+                return True
+            if request.user.role == 3:
+                try:
+                    r = (obj in [c for c in Contract.objects
+                                            .filter(event__support_contact=request.user)])
+                except ValueError():
+                    r = False
+                return r
+
     def has_object_permission(self, request, view, obj):
         return (request.user.role == 1)
