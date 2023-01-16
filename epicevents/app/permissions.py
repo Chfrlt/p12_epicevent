54d63d2b809dd92da4659675943711461ed2baf0
Charles <charlesferlet@gmail.com>
2023-01-16 01:02:13 +0100

    feat: Add manager permissions

--- epicevents/app/permissions.py
+++ epicevents/app/permissions.py
@@ -1,10 +1,15 @@
 from rest_framework import permissions
 from rest_framework.exceptions import PermissionDenied
 
+from .models import Client, Contract, Event, User
+
 
 class IsManager():
     def has_permission(self, request, view):
-        return (request.user.role == 1)
+        return request.user.role == 1
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.role == 1
 
     def has_object_permission(self, request, view, obj):
         return (request.user.role == 1)
