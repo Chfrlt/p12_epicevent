e0cfd66e984888f6009280d49994aaced1defee5
Charles <charlesferlet@gmail.com>
2023-01-18 22:55:35 +0100

    fix; fix permissions for contracts
    This changes the permissions so that sales member are not allowed to update a contract if they are not the contact of the client linked to the contract.

--- epicevents/app/permissions.py
+++ epicevents/app/permissions.py
@@ -35,13 +35,10 @@ class ContractPermissions(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         if request.method in ('PATCH', 'PUT') or request.method in permissions.SAFE_METHODS:
-            if request.user.role == User.SALES:
-                return True
-            if request.user.role == User.SUPPORT:
-                return (obj in [c for c in Contract.objects
-                                            .filter(event__support_contact=request.user)])
-        if request.method == 'DELETE':
-            return False
+            return (obj.client.sales_contact == request.user or
+                    obj in [c for c in Contract.objects
+                                               .filter(event__support_contact=request.user)])
+
 
 class EventPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
